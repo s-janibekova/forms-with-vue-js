3a7d7e49b00e6ea40bb824a8e7ac4e1ef7e0ac94
S_Janybekova <s.t.janibekova@gmail.com>
2020-01-08 14:39:47 +0600

    first commit

---
+++ forms-in-vuejs/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <basic-form />
+    </div>
+</template>
+
+<script>
+import BasicForm from './components/BasicForm.vue'
+export default {
+    name: 'app',
+    components: {
+        BasicForm
+    }
+};
+</script>
